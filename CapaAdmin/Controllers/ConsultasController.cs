6048801fc9151b7e7f6b6908f39a9d0cb71f16c1
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-03-27 22:36:47 -0400

    type: layout, controladores y vistas -- CapaAdmin

---
+++ CapaAdmin/Controllers/ConsultasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaAdmin.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        public ActionResult ConsultaDepartamento()
+        {
+            return View();
+        }
+
+        public ActionResult ConsultaPuesto()
+        {
+            return View();
+        }
+    }
+}
