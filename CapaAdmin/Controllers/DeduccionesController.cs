4c97c37a43a6707edf14c9233d586714351d7bb7
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-04-13 00:13:52 -0400

    type: mapeado de la DB, avances en gest. deducciones

---
+++ CapaAdmin/Controllers/DeduccionesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaAdmin.Models;
+using CapaAdmin.Models.ViewModel;
+
+namespace CapaAdmin.Controllers
+{
+    public class DeduccionesController : Controller
+    {
+        // GET: Deducciones
+        public ActionResult Deducciones()
+        {
+            List<DataGestionDeducciones> lst;
+            using (FENIX_NOMINAEntities db= new FENIX_NOMINAEntities()) {
+
+                lst = (from d in db.TP_DEDUCCIONES
+                           select new DataGestionDeducciones
+                           {
+                               ID_TPDEDU = d.ID_TPDEDU,
+                               NOMBRE = d.NOMBRE,
+                               DEPENDE_SALARIO = d.DEPENDE_SALARIO,
+                               ESTADO = d.ESTADO,
+                           }).ToList();
+            }
+
+            return View(lst);
+        }
+    }
+}
