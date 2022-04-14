3811f0aea4483afb8c16e0fbf2eaf807a6d129fa
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-04-13 23:25:47 -0400

    type: avance en el crud de gest Deducciones - erro al insertar datos

--- CapaAdmin/Controllers/DeduccionesController.cs
+++ CapaAdmin/Controllers/DeduccionesController.cs
@@ -10,23 +10,60 @@ namespace CapaAdmin.Controllers
 {
     public class DeduccionesController : Controller
     {
+        
+
         // GET: Deducciones
         public ActionResult Deducciones()
         {
             List<DataGestionDeducciones> lst;
-            using (FENIX_NOMINAEntities db= new FENIX_NOMINAEntities()) {
+            using (FENIX_NOMINAEntities db = new FENIX_NOMINAEntities()) {
 
                 lst = (from d in db.TP_DEDUCCIONES
-                           select new DataGestionDeducciones
-                           {
-                               ID_TPDEDU = d.ID_TPDEDU,
-                               NOMBRE = d.NOMBRE,
-                               DEPENDE_SALARIO = d.DEPENDE_SALARIO,
-                               ESTADO = d.ESTADO,
-                           }).ToList();
+                       select new DataGestionDeducciones
+                       {
+                           ID_TPDEDU = d.ID_TPDEDU,
+                           NOMBRE = d.NOMBRE,
+                           DEPENDE_SALARIO = d.DEPENDE_SALARIO,
+                           ESTADO = d.ESTADO,
+                       }).ToList();
             }
 
             return View(lst);
+        }
+
+
+        public ActionResult CrearDeduccion() {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CrearDeduccion(DataGestionDeduccionesLlenar model) {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (FENIX_NOMINAEntities db = new FENIX_NOMINAEntities())
+                    {
+                        var oDEDUCCIONES = new TP_DEDUCCIONES();
+                        oDEDUCCIONES.NOMBRE = model.NOMBRE;
+                        oDEDUCCIONES.DEPENDE_SALARIO = model.DEPENDE_SALARIO;
+                        oDEDUCCIONES.ESTADO = model.ESTADO;
+
+                        db.TP_DEDUCCIONES.Add(oDEDUCCIONES);
+                        db.SaveChanges();
+                    }
+                    return  Redirect("/Deducciones"); 
+                }
+
+                return View(model);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
